860ac655a203b7ab3838114d61af4c02d8b1e225
Perales33 <perales.antonio33@hotmail.com>
2024-08-25 18:13:18 +0200

    eliminación js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Hacer que se muestre la alerta cuando se completa el formulario
-
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert("¡Gracias por contactar a la Alianza Rebelde! Nos pondremos en contacto contigo pronto.");
-});
